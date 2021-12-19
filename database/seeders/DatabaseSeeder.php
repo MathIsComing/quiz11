fa64e428855aa86d185a73ea01c197227924970e
MathIsComing <oguzhan.5055@gmail.com>
2021-12-19 13:58:17 +0300

    seeder ayarı ayrı ayrı tablolara bölündü yapıld. factory seed oluşturuldu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        
-        User::insert([
-            'name' => "Oguzhan",
-            'email' => "oguzhan.5055@gmail.com",
-            "type"=>"admin",
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+       $this->call([
+        UserSeeder::class,
+        QuizSeeder::class,
 
-        ]);
-        \App\Models\User::factory(5)->create();
+
+       ]); 
+       
     }
 }
